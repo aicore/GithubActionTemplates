545d05f98670f80c66a0243b89a2fa14cf0c840d
abose <arun@core.ai>
2021-07-04 23:01:36 +0530

    detecting all files changed in the pull request

--- .github/workflows/python/pull_request_details.py
+++ .github/workflows/python/pull_request_details.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import subprocess
 from github import Github
 # We use https://github.com/PyGithub/PyGithub for github api interfacing
 
@@ -22,12 +23,22 @@ print("Number of commits in the pull request: ", len(commit_details))
 # github logins of all committers
 commit_logins = []
 commit_id_list = []
+files_updated = []
 for commit in commit_details:
     commiter_github_login = commit['committer']['login']
-    commit_id_list.append(commit['sha'])
     if commiter_github_login not in commit_logins:
         commit_logins.append(commiter_github_login)
+    
+    commit_id = commit['sha']
+    commit_id_list.append(commit_id)
+
+    files = subprocess.check_output(
+        ['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', commit_id]).decode('utf-8').splitlines()
+    for file in files:
+        if file not in files_updated:
+            files_updated.append(file)
 
 print("All github users who made changes to the pull request: ", commit_logins)
 print("All commit ids in pull request: ", commit_id_list)
+print("All files updated in pull request: ", files_updated)
 
