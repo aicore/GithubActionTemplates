5c6f30b320abdd7d70a17dad12e7c0cc5b9609db
abose <arun@core.ai>
2021-07-04 23:33:28 +0530

    exec git commands from python

--- .github/workflows/python/pull_request_details.py
+++ .github/workflows/python/pull_request_details.py
@@ -20,13 +20,20 @@ if github_details["event_name"] != "pull_request":
 print("Pull request submitted by github login: ", github_details['event']['pull_request']['user']['login'])
 print("Number of commits in the pull request: ", len(commit_details))
 
+# Check if current dir is git dir
+is_git_dir = subprocess.check_output(
+        ['/usr/bin/git', 'rev-parse', '--is-inside-work-tree']).decode('utf-8')
+print("Is git dir: ", is_git_dir)
+
+# 
+last_n_commit_list = subprocess.check_output(
+        ['/usr/bin/git', 'rev-list', '--max-count=10', 'HEAD']).decode('utf-8')
+print("last 10 commit ids are: ", last_n_commit_list)
+
 # github logins of all committers
 commit_logins = []
 commit_id_list = []
 files_updated = []
-is_git_dir = subprocess.check_output(
-        ['/usr/bin/git', 'rev-parse', '--is-inside-work-tree']).decode('utf-8')
-print("is git dir: ", is_git_dir)
 for commit in commit_details:
     commiter_github_login = commit['committer']['login']
     if commiter_github_login not in commit_logins:
