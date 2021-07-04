7faca539941563a970ea073e73fd3700e4c7cf38
abose <arun@core.ai>
2021-07-04 22:16:58 +0530

    Getting commit details

--- .github/workflows/python/pull_request_details.py
+++ .github/workflows/python/pull_request_details.py
@@ -1,7 +1,16 @@
 import os
+import sys
+import json
 from github import Github
+# We use https://github.com/PyGithub/PyGithub for github api interfacing
 
 print("current working directory is: ", os.getcwd())
 
-with open('./.tmp/github.json', 'r') as f:
-    print(f.read())
+github_info_file = open('./.tmp/github.json', 'r') 
+github_details = json.load(github_info_file)
+
+if github_details["event_name"] != "pull_request":
+    print("Error! This operation is valid on github pull requests. Exiting")
+    sys.exit(1)
+
+print("Pull request submitted by github login: ", github_details['event']['pull_request']['user']['login'])
