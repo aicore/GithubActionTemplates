9d0162ab212646fe37b62cb4c24e436f63cf8910
abose <arun@core.ai>
2021-07-04 19:43:23 +0530

    runnin python script on pull request sample

--- test_no_exe_files.py
+++ test_no_exe_files.py
@@ -1,7 +1,9 @@
 import sys, os
 
 def test_no_exe_files():
+    print("Searching for exe files in ", os.getcwd())
     for fname in os.listdir('.'):
+        # non recursive search
         if fname.endswith('.exe'):
             sys.exit(1)
             break
